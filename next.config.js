eb33c390bf36af0e583c1950de982b07e462a01a
Fergal Moran <fergal.moran@gmail.com>
2024-09-13 18:55:29 +0100

    Add github avis to next image

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    domains: ["localhost"],
+    remotePatterns: [
+      {
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+};
 
 export default config;
